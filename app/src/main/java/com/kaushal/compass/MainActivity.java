08a873c418775d9f23ec33180c97fbcaa05c2124
kdontula <kdontula@hawk.iit.edu>
2016-08-05 18:29:39 -0500

    test

--- app/src/main/java/com/kaushal/compass/MainActivity.java
+++ app/src/main/java/com/kaushal/compass/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
